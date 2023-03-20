77b3f3efc28de47dc1a516a7a30c36d9b4c5592b
kyoding <minkyo0816@gmail.com>
2023-03-20 15:15:18 +0900

    0320

---
+++ mybatisProject/src/com/kh/mybatis/member/model/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.kh.mybatis.member.model.service;
+
+import com.kh.mybatis.member.model.vo.Member;
+
+public interface MemberService {
+	
+	// 인터페이스 : 상수필드(묵시적으로 public static final) + 추상메소드만(묵시적으로 public abstract) 쓸 수 있음!!
+	
+	/*public abstract*/ int insertMember(Member m);
+	
+	Member loginMember(Member m);
+	
+	int updateMember(Member m);
+	
+	int deleteMember(String userId);
+
+}
